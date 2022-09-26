cc285872b395435f86a947512012582c327a40b2
Katerina Tang <kbt28@cornell.edu>
2022-09-26 18:30:09 -0400

    Restructure code

--- DO_conc_simulation.jl
+++ DO_conc_simulation.jl
@@ -1,6 +1,10 @@
-using Plots
+using Plots, Distributions, PDMats
 
-function calc_DO_concentration(x, C0, B0, N0)
+function calc_mixed_concentration(V0, C0, V1, C1)
+    return (V0 * C0 + V1 * C1)/(V0 + V1)
+end
+
+function calc_concentrations(x, C0, B0, N0)
     ka = 0.55
     kc = 0.35
     kn = 0.25
@@ -11,51 +15,105 @@ function calc_DO_concentration(x, C0, B0, N0)
     α2 = kc * (exp(-kc*x / U) - exp(-ka*x / U)) / (ka - kc)
     α3 = kn * (exp(-kn*x / U) - exp(-ka*x / U)) / (ka - kn)
 
-    conc = Cs*(1-α1) + C0*α1 - B0*α2 -N0*α3
+    C = Cs*(1-α1) + C0*α1 - B0*α2 - N0*α3
+    B = B0*exp(-kc*(x/U))
+    N = N0*exp(-kn*(x/U))
 
-    return conc
+    return C, B, N
 end
 
-function DO_concentration(E1 = 0, E2 = 0, max_dist = 50)
-    conc = zeros(max_dist + 1)
+function DO_simulation(E1 = 0, E2 = 0, max_dist = 50)
+    C = zeros(max_dist + 1)
+    B = zeros(max_dist + 1)
+    N = zeros(max_dist + 1)
 
-    kc = 0.35
-    kn = 0.25
-    U = 6
-
-    C0 = (750 + 50)/110
-    B0 = (500 + (1 - E1)*500)/110
-    N0 = (500 + (1 - E1)*350)/110
-
-    C15 = (110*calc_DO_concentration(15, C0, B0, N0) + 75)/125
-    B15 = (110*B0*exp(-kc*(15/U)) + (1 - E2)*675)/125
-    N15 = (110*N0*exp(-kn*(15/U)) + (1 - E2)*525)/125
+    C[1] = calc_mixed_concentration(1e5, 7.5, 1e4, 5)
+    B[1] = calc_mixed_concentration(1e5, 5, 1e4, (1 - E1)*50)
+    N[1] = calc_mixed_concentration(1e5, 5, 1e4, (1 - E1)*35)
     
-    for x = 0:(max_dist)
+    for x = 1:(max_dist)
         if x < 15
-            conc[x + 1] = calc_DO_concentration(x, C0, B0, N0)
+            C[x + 1], B[x + 1], N[x + 1] = calc_concentrations(x, C[1], B[1], N[1])
+        elseif x == 15
+            C[16] = calc_mixed_concentration(1.1e5, C[15], 1.5e4, 5)
+            B[16] = calc_mixed_concentration(1.1e5, B[15], 1.5e4, (1 - E2)*45)
+            N[16] = calc_mixed_concentration(1.1e5, N[15], 1.5e4, (1 - E2)*35)
         else
-            conc[x + 1] = calc_DO_concentration(x - 15, C15, B15, N15)
+            C[x + 1], B[x + 1], N[x + 1] = calc_concentrations(x - 15, C[16], B[16], N[16])
         end
     end
 
-    return conc
+    return C, B, N
 end
 
-conc = DO_concentration(0, 0, 50)
-plot(0:50, conc, yaxis=[3, 7.5], label=false, xlabel="Distance (km)", ylabel="DO concentration (mg/L)", linewidth=2)
-vline!([0, 15], label="Waste discharge", linewidth=2)
+function stochastic_DO_simulation1(E1 = 0, E2 = 0, max_dist = 50)
+    C = zeros(max_dist + 1)
+    B = zeros(max_dist + 1)
+    N = zeros(max_dist + 1)
 
-# findall(x -> x >= 6.0, conc)[findfirst(findall(x -> x >= 6.0, conc) .>= 16)] -1
+    C[1] = calc_mixed_concentration(1e5, 7.5, 1e4, 5)
+    B[1] = calc_mixed_concentration(1e5, rand(Uniform(4, 7)), 1e4, (1 - E1)*50)
+    N[1] = calc_mixed_concentration(1e5, rand(Uniform(3, 8)), 1e4, (1 - E1)*35)
+    
+    for x = 1:(max_dist)
+        if x < 15
+            C[x + 1], B[x + 1], N[x + 1] = calc_concentrations(x, C[1], B[1], N[1])
+        elseif x == 15
+            C[16] = calc_mixed_concentration(1.1e5, C[15], 1.5e4, 5)
+            B[16] = calc_mixed_concentration(1.1e5, B[15], 1.5e4, (1 - E2)*45)
+            N[16] = calc_mixed_concentration(1.1e5, N[15], 1.5e4, (1 - E2)*35)
+        else
+            C[x + 1], B[x + 1], N[x + 1] = calc_concentrations(x - 15, C[16], B[16], N[16])
+        end
+    end
 
-test_E2s = 0:0.01:1.0
-min_DO_conc = zeros(length(test_E2s))
-i = 1
-for E2 in test_E2s
-    # conc = DO_concentration(0, E2, 50)
-    conc = DO_concentration(E2, E2, 50)
-    min_DO_conc[i] = minimum(conc[16:51])
-    i += 1
+    return C, B, N
 end
-plot(test_E2s, min_DO_conc, label=false, xlabel="Treatment level (% removal)", ylabel="Minimum DO concentration (mg/L)", linewidth=2)
-hline!([4.0], label="Target", linewidth=2)
+
+min_DO_conc = zeros(1000)
+for n in 1:1000
+    DO_conc, CBOD_conc, NBOD_conc = stochastic_DO_simulation1(0.15, 0.15, 50)
+    min_DO_conc[n] = minimum(DO_conc)
+end
+sum(min_DO_conc .>= 4.0)/10
+
+function sample_correlated_uniform(n, a, b, corr_coef=0.7)
+    mvnorm = MvNormal([0, 0], PDMat([1 corr_coef; corr_coef 1])) # set up a multivariate normal with each marginal variance of 1 and the right correlation
+    norm_samples = rand(mvnorm, n)' # sample from the multivariate normal, the marginal distributions are a standard normal
+    unif_samples = cdf.(Normal(0, 1), norm_samples) # convert samples to a uniform distribution using the pdf of a standard Normal
+    samples = (unif_samples .* [a[2] - a[1] b[2] - b[1]]) .+ [a[1] b[1]]
+    return samples
+end
+
+function stochastic_DO_simulation2(E1 = 0, E2 = 0, max_dist = 50)
+    C = zeros(max_dist + 1)
+    B = zeros(max_dist + 1)
+    N = zeros(max_dist + 1)
+
+    inflow_org_matter = sample_correlated_uniform(1, [4, 7], [3, 8])
+
+    C[1] = calc_mixed_concentration(1e5, 7.5, 1e4, 5)
+    B[1] = calc_mixed_concentration(1e5, inflow_org_matter[1], 1e4, (1 - E1)*50)
+    N[1] = calc_mixed_concentration(1e5, inflow_org_matter[2], 1e4, (1 - E1)*35)
+    
+    for x = 1:(max_dist)
+        if x < 15
+            C[x + 1], B[x + 1], N[x + 1] = calc_concentrations(x, C[1], B[1], N[1])
+        elseif x == 15
+            C[16] = calc_mixed_concentration(1.1e5, C[15], 1.5e4, 5)
+            B[16] = calc_mixed_concentration(1.1e5, B[15], 1.5e4, (1 - E2)*45)
+            N[16] = calc_mixed_concentration(1.1e5, N[15], 1.5e4, (1 - E2)*35)
+        else
+            C[x + 1], B[x + 1], N[x + 1] = calc_concentrations(x - 15, C[16], B[16], N[16])
+        end
+    end
+
+    return C, B, N
+end
+
+min_DO_conc = zeros(1000)
+for n in 1:1000
+    DO_conc, CBOD_conc, NBOD_conc = stochastic_DO_simulation2(0.2, 0.2, 50)
+    min_DO_conc[n] = minimum(DO_conc)
+end
+sum(min_DO_conc .>= 4.0)/10
